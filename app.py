e7becc9687122c91dc84aedd89b461688ae0e8f7
jyotsna48 <24r21a6610@mlrit.ac.in>
2026-07-04 00:34:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("AI Learning Buddy 🚀")
+
+st.write("Your Streamlit app is working!")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.success(f"Hello {name} 👋 Welcome to your AI buddy!")
